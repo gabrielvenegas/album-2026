6a4c1b3131240faa1701f8cf1b4a9df4d78bd056
Gabriel Enrique <ged.venegas@gmail.com>
2026-05-17 16:55:33 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <div className="app-shell">
-        <div className="flex-1 flex flex-col min-h-0">
+        <div className="app-content flex-1 flex flex-col min-h-0">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/paises" element={<Countries />} />
