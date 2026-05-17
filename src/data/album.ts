87875b3dc5d75eb312397f3ab67ba88242c6c798
Gabriel Enrique <ged.venegas@gmail.com>
2026-05-17 18:48:49 -0300

    wip

--- src/data/album.ts
+++ src/data/album.ts
@@ -24,89 +24,80 @@ function makeStickers(count = 20): StickerDef[] {
   }))
 }
 
-// Groups A–L as listed in the Panini FIFA World Cup 2026 album
-// Group A: México, África do Sul, Coreia do Sul, República Tcheca
-// Group B: Canadá, Qatar, Suíça, Nova Zelândia
-// Group C: Estados Unidos, Escócia, Haiti, Brasil
-// Group D: Austrália, Turquia, Chile (+ Escócia*)
-// Group E: Inglaterra, Croácia, Gana, Panamá
-// Group F: França, Ucrânia, Mali (+ Nova Zelândia*)
-// Group G: Alemanha, Irlanda, Jamaica, Uzbequistão
-// Group H: Espanha, Romênia, Egito, Honduras
-// Group I: Itália, Noruega, Costa do Marfim (+ Canadá*)
-// Group J: Argentina, Argélia, Áustria, Jordânia
-// Group K: Portugal, RD do Congo, Colômbia, Japão
-// Group L: Holanda, Uruguai, Emirados Árabes Unidos, Fiji
-
+// Groups A-L as listed in the Panini FIFA World Cup 2026 album.
+// Each country currently has 20 sticker slots.
 export const COUNTRIES: Country[] = [
   // Group A
-  { code: 'MEX', name: 'México',            flag: '🇲🇽', confederation: 'CONCACAF', group: 'A', stickers: makeStickers() },
-  { code: 'RSA', name: 'África do Sul',     flag: '🇿🇦', confederation: 'CAF',      group: 'A', stickers: makeStickers() },
-  { code: 'KOR', name: 'Coreia do Sul',     flag: '🇰🇷', confederation: 'AFC',      group: 'A', stickers: makeStickers() },
-  { code: 'CZE', name: 'República Tcheca',  flag: '🇨🇿', confederation: 'UEFA',     group: 'A', stickers: makeStickers() },
+  { code: 'MEX', name: 'México',             flag: '🇲🇽', confederation: 'CONCACAF', group: 'A', stickers: makeStickers() },
+  { code: 'RSA', name: 'África do Sul',      flag: '🇿🇦', confederation: 'CAF',      group: 'A', stickers: makeStickers() },
+  { code: 'KOR', name: 'Coreia do Sul',      flag: '🇰🇷', confederation: 'AFC',      group: 'A', stickers: makeStickers() },
+  { code: 'CZE', name: 'Tchéquia',           flag: '🇨🇿', confederation: 'UEFA',     group: 'A', stickers: makeStickers() },
 
   // Group B
-  { code: 'CAN', name: 'Canadá',            flag: '🇨🇦', confederation: 'CONCACAF', group: 'B', stickers: makeStickers() },
-  { code: 'QAT', name: 'Qatar',             flag: '🇶🇦', confederation: 'AFC',      group: 'B', stickers: makeStickers() },
-  { code: 'SUI', name: 'Suíça',             flag: '🇨🇭', confederation: 'UEFA',     group: 'B', stickers: makeStickers() },
-  { code: 'NZL', name: 'Nova Zelândia',     flag: '🇳🇿', confederation: 'OFC',      group: 'B', stickers: makeStickers() },
+  { code: 'CAN', name: 'Canadá',             flag: '🇨🇦', confederation: 'CONCACAF', group: 'B', stickers: makeStickers() },
+  { code: 'BIH', name: 'Bósnia-Herzegovina', flag: '🇧🇦', confederation: 'UEFA',     group: 'B', stickers: makeStickers() },
+  { code: 'QAT', name: 'Qatar',              flag: '🇶🇦', confederation: 'AFC',      group: 'B', stickers: makeStickers() },
+  { code: 'SUI', name: 'Suíça',              flag: '🇨🇭', confederation: 'UEFA',     group: 'B', stickers: makeStickers() },
 
   // Group C
-  { code: 'USA', name: 'Estados Unidos',    flag: '🇺🇸', confederation: 'CONCACAF', group: 'C', stickers: makeStickers() },
-  { code: 'SCO', name: 'Escócia',           flag: '🏴󠁧󠁢󠁳󠁣󠁴󠁿', confederation: 'UEFA',     group: 'C', stickers: makeStickers() },
-  { code: 'HAI', name: 'Haiti',             flag: '🇭🇹', confederation: 'CONCACAF', group: 'C', stickers: makeStickers() },
-  { code: 'BRA', name: 'Brasil',            flag: '🇧🇷', confederation: 'CONMEBOL', group: 'C', stickers: makeStickers() },
+  { code: 'BRA', name: 'Brasil',             flag: '🇧🇷', confederation: 'CONMEBOL', group: 'C', stickers: makeStickers() },
+  { code: 'MAR', name: 'Marrocos',           flag: '🇲🇦', confederation: 'CAF',      group: 'C', stickers: makeStickers() },
+  { code: 'HAI', name: 'Haiti',              flag: '🇭🇹', confederation: 'CONCACAF', group: 'C', stickers: makeStickers() },
+  { code: 'SCO', name: 'Escócia',            flag: '🏴',  confederation: 'UEFA',     group: 'C', stickers: makeStickers() },
 
   // Group D
-  { code: 'AUS', name: 'Austrália',         flag: '🇦🇺', confederation: 'AFC',      group: 'D', stickers: makeStickers() },
-  { code: 'TUR', name: 'Turquia',           flag: '🇹🇷', confederation: 'UEFA',     group: 'D', stickers: makeStickers() },
-  { code: 'CHI', name: 'Chile',             flag: '🇨🇱', confederation: 'CONMEBOL', group: 'D', stickers: makeStickers() },
+  { code: 'USA', name: 'Estados Unidos',     flag: '🇺🇸', confederation: 'CONCACAF', group: 'D', stickers: makeStickers() },
+  { code: 'PAR', name: 'Paraguai',           flag: '🇵🇾', confederation: 'CONMEBOL', group: 'D', stickers: makeStickers() },
+  { code: 'AUS', name: 'Austrália',          flag: '🇦🇺', confederation: 'AFC',      group: 'D', stickers: makeStickers() },
+  { code: 'TUR', name: 'Turquia',            flag: '🇹🇷', confederation: 'UEFA',     group: 'D', stickers: makeStickers() },
 
   // Group E
-  { code: 'ENG', name: 'Inglaterra',        flag: '🏴󠁧󠁢󠁥󠁮󠁧󠁿', confederation: 'UEFA',     group: 'E', stickers: makeStickers() },
-  { code: 'CRO', name: 'Croácia',           flag: '🇭🇷', confederation: 'UEFA',     group: 'E', stickers: makeStickers() },
-  { code: 'GHA', name: 'Gana',              flag: '🇬🇭', confederation: 'CAF',      group: 'E', stickers: makeStickers() },
-  { code: 'PAN', name: 'Panamá',            flag: '🇵🇦', confederation: 'CONCACAF', group: 'E', stickers: makeStickers() },
+  { code: 'GER', name: 'Alemanha',           flag: '🇩🇪', confederation: 'UEFA',     group: 'E', stickers: makeStickers() },
+  { code: 'CUW', name: 'Curaçao',            flag: '🇨🇼', confederation: 'CONCACAF', group: 'E', stickers: makeStickers() },
+  { code: 'CIV', name: 'Costa do Marfim',    flag: '🇨🇮', confederation: 'CAF',      group: 'E', stickers: makeStickers() },
+  { code: 'ECU', name: 'Equador',            flag: '🇪🇨', confederation: 'CONMEBOL', group: 'E', stickers: makeStickers() },
 
   // Group F
-  { code: 'FRA', name: 'França',            flag: '🇫🇷', confederation: 'UEFA',     group: 'F', stickers: makeStickers() },
-  { code: 'UKR', name: 'Ucrânia',           flag: '🇺🇦', confederation: 'UEFA',     group: 'F', stickers: makeStickers() },
-  { code: 'MLI', name: 'Mali',              flag: '🇲🇱', confederation: 'CAF',      group: 'F', stickers: makeStickers() },
+  { code: 'NED', name: 'Holanda',            flag: '🇳🇱', confederation: 'UEFA',     group: 'F', stickers: makeStickers() },
+  { code: 'JPN', name: 'Japão',              flag: '🇯🇵', confederation: 'AFC',      group: 'F', stickers: makeStickers() },
+  { code: 'SWE', name: 'Suécia',             flag: '🇸🇪', confederation: 'UEFA',     group: 'F', stickers: makeStickers() },
+  { code: 'TUN', name: 'Tunísia',            flag: '🇹🇳', confederation: 'CAF',      group: 'F', stickers: makeStickers() },
 
   // Group G
-  { code: 'GER', name: 'Alemanha',          flag: '🇩🇪', confederation: 'UEFA',     group: 'G', stickers: makeStickers() },
-  { code: 'IRL', name: 'Irlanda',           flag: '🇮🇪', confederation: 'UEFA',     group: 'G', stickers: makeStickers() },
-  { code: 'JAM', name: 'Jamaica',           flag: '🇯🇲', confederation: 'CONCACAF', group: 'G', stickers: makeStickers() },
-  { code: 'UZB', name: 'Uzbequistão',       flag: '🇺🇿', confederation: 'AFC',      group: 'G', stickers: makeStickers() },
+  { code: 'BEL', name: 'Bélgica',            flag: '🇧🇪', confederation: 'UEFA',     group: 'G', stickers: makeStickers() },
+  { code: 'EGY', name: 'Egito',              flag: '🇪🇬', confederation: 'CAF',      group: 'G', stickers: makeStickers() },
+  { code: 'IRN', name: 'Irã',                flag: '🇮🇷', confederation: 'AFC',      group: 'G', stickers: makeStickers() },
+  { code: 'NZL', name: 'Nova Zelândia',      flag: '🇳🇿', confederation: 'OFC',      group: 'G', stickers: makeStickers() },
 
   // Group H
-  { code: 'ESP', name: 'Espanha',           flag: '🇪🇸', confederation: 'UEFA',     group: 'H', stickers: makeStickers() },
-  { code: 'ROU', name: 'Romênia',           flag: '🇷🇴', confederation: 'UEFA',     group: 'H', stickers: makeStickers() },
-  { code: 'EGY', name: 'Egito',             flag: '🇪🇬', confederation: 'CAF',      group: 'H', stickers: makeStickers() },
-  { code: 'HON', name: 'Honduras',          flag: '🇭🇳', confederation: 'CONCACAF', group: 'H', stickers: makeStickers() },
+  { code: 'ESP', name: 'Espanha',            flag: '🇪🇸', confederation: 'UEFA',     group: 'H', stickers: makeStickers() },
+  { code: 'CPV', name: 'Cabo Verde',         flag: '🇨🇻', confederation: 'CAF',      group: 'H', stickers: makeStickers() },
+  { code: 'KSA', name: 'Arábia Saudita',     flag: '🇸🇦', confederation: 'AFC',      group: 'H', stickers: makeStickers() },
+  { code: 'URU', name: 'Uruguai',            flag: '🇺🇾', confederation: 'CONMEBOL', group: 'H', stickers: makeStickers() },
 
   // Group I
-  { code: 'ITA', name: 'Itália',            flag: '🇮🇹', confederation: 'UEFA',     group: 'I', stickers: makeStickers() },
-  { code: 'NOR', name: 'Noruega',           flag: '🇳🇴', confederation: 'UEFA',     group: 'I', stickers: makeStickers() },
-  { code: 'CIV', name: 'Costa do Marfim',   flag: '🇨🇮', confederation: 'CAF',      group: 'I', stickers: makeStickers() },
+  { code: 'FRA', name: 'França',             flag: '🇫🇷', confederation: 'UEFA',     group: 'I', stickers: makeStickers() },
+  { code: 'SEN', name: 'Senegal',            flag: '🇸🇳', confederation: 'CAF',      group: 'I', stickers: makeStickers() },
+  { code: 'IRQ', name: 'Iraque',             flag: '🇮🇶', confederation: 'AFC',      group: 'I', stickers: makeStickers() },
+  { code: 'NOR', name: 'Noruega',            flag: '🇳🇴', confederation: 'UEFA',     group: 'I', stickers: makeStickers() },
 
   // Group J
-  { code: 'ARG', name: 'Argentina',         flag: '🇦🇷', confederation: 'CONMEBOL', group: 'J', stickers: makeStickers() },
-  { code: 'ALG', name: 'Argélia',           flag: '🇩🇿', confederation: 'CAF',      group: 'J', stickers: makeStickers() },
-  { code: 'AUT', name: 'Áustria',           flag: '🇦🇹', confederation: 'UEFA',     group: 'J', stickers: makeStickers() },
-  { code: 'JOR', name: 'Jordânia',          flag: '🇯🇴', confederation: 'AFC',      group: 'J', stickers: makeStickers() },
+  { code: 'ARG', name: 'Argentina',          flag: '🇦🇷', confederation: 'CONMEBOL', group: 'J', stickers: makeStickers() },
+  { code: 'ALG', name: 'Argélia',            flag: '🇩🇿', confederation: 'CAF',      group: 'J', stickers: makeStickers() },
+  { code: 'AUT', name: 'Áustria',            flag: '🇦🇹', confederation: 'UEFA',     group: 'J', stickers: makeStickers() },
+  { code: 'JOR', name: 'Jordânia',           flag: '🇯🇴', confederation: 'AFC',      group: 'J', stickers: makeStickers() },
 
   // Group K
-  { code: 'POR', name: 'Portugal',          flag: '🇵🇹', confederation: 'UEFA',     group: 'K', stickers: makeStickers() },
-  { code: 'COD', name: 'RD do Congo',       flag: '🇨🇩', confederation: 'CAF',      group: 'K', stickers: makeStickers() },
-  { code: 'COL', name: 'Colômbia',          flag: '🇨🇴', confederation: 'CONMEBOL', group: 'K', stickers: makeStickers() },
-  { code: 'JPN', name: 'Japão',             flag: '🇯🇵', confederation: 'AFC',      group: 'K', stickers: makeStickers() },
+  { code: 'POR', name: 'Portugal',           flag: '🇵🇹', confederation: 'UEFA',     group: 'K', stickers: makeStickers() },
+  { code: 'COD', name: 'RD do Congo',        flag: '🇨🇩', confederation: 'CAF',      group: 'K', stickers: makeStickers() },
+  { code: 'UZB', name: 'Uzbequistão',        flag: '🇺🇿', confederation: 'AFC',      group: 'K', stickers: makeStickers() },
+  { code: 'COL', name: 'Colômbia',           flag: '🇨🇴', confederation: 'CONMEBOL', group: 'K', stickers: makeStickers() },
 
   // Group L
-  { code: 'NED', name: 'Holanda',           flag: '🇳🇱', confederation: 'UEFA',     group: 'L', stickers: makeStickers() },
-  { code: 'URU', name: 'Uruguai',           flag: '🇺🇾', confederation: 'CONMEBOL', group: 'L', stickers: makeStickers() },
-  { code: 'UAE', name: 'Emirados Árabes',   flag: '🇦🇪', confederation: 'AFC',      group: 'L', stickers: makeStickers() },
-  { code: 'FIJ', name: 'Fiji',              flag: '🇫🇯', confederation: 'OFC',      group: 'L', stickers: makeStickers() },
+  { code: 'ENG', name: 'Inglaterra',         flag: '🏴',  confederation: 'UEFA',     group: 'L', stickers: makeStickers() },
+  { code: 'CRO', name: 'Croácia',            flag: '🇭🇷', confederation: 'UEFA',     group: 'L', stickers: makeStickers() },
+  { code: 'GHA', name: 'Gana',               flag: '🇬🇭', confederation: 'CAF',      group: 'L', stickers: makeStickers() },
+  { code: 'PAN', name: 'Panamá',             flag: '🇵🇦', confederation: 'CONCACAF', group: 'L', stickers: makeStickers() },
 ]
 
 export const TOTAL_STICKERS = COUNTRIES.reduce((sum, c) => sum + c.stickers.length, 0)
